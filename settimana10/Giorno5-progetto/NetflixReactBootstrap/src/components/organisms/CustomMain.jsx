646ef322c24811461b1c397cbcbeb0a13d93d3d8
CCiocca <chiara.cioccarelli1@gmail.com>
2024-01-11 18:02:50 +0100

    udp2

--- settimana10/Giorno5-progetto/NetflixReactBootstrap/src/components/organisms/CustomMain.jsx
+++ settimana10/Giorno5-progetto/NetflixReactBootstrap/src/components/organisms/CustomMain.jsx
@@ -29,7 +29,7 @@ const CustomMain = () => {
             <Col className='d-flex justify-content-end text-secondary'>
                 <div className="border p-2 px-3 d-flex align-items-center">
                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list-nested" viewBox="0 0 16 16">
-                           <path fill-rule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5"/>
+                           <path fillRule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5"/>
                          </svg>
                 </div>
 
